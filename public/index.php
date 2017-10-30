adfc184b238963a8c2c98916e59debccda8394e0
sarere <samuel.r@ti.ukdw.ac.id>
2017-10-30 23:31:47 +0700

    test link

--- public/index.php
+++ public/index.php
@@ -5,9 +5,8 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+$app->get('/hello', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello");
 
     return $response;
 });
