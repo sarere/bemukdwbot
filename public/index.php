fecaee84bc0f0427aacd68e319b50f5ab9f27b08
sarere <samuel.r@ti.ukdw.ac.id>
2017-10-31 00:16:04 +0700

    tes

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require('../vendor/autoload.php');
 
-$app = new \Slim\App;
+$app = new Slim\App();
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
